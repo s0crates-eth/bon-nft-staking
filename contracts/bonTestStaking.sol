20e837ca3acc2d237af6e752cd45075ddab73af3
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-02-15 20:03:32 +0700

    Update bonTestStaking.sol

--- contracts/bonTestStaking.sol
+++ contracts/bonTestStaking.sol
@@ -5,7 +5,7 @@ https://chainstack.com/deploying-an-nft-staking-contract-on-gnosis-chain/
 
 - this should have more emits that can be used as JS triggers
 - check the gas data for emits; too costly?
-- x
+- add a small dapp tax
 
 */
 
